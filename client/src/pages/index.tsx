8f2fb5dcfae47cfacac20a96e802640a6ff8c9d5
mjcarnaje <michaeljamescarnaje1@gmail.com>
2020-12-24 17:17:35 +0800

    apollo client setup, set false cors to the server, package.json server fixed

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,56 +1,62 @@
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
 import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+	Code,
+	Link as ChakraLink,
+	List,
+	ListIcon,
+	ListItem,
+	Text,
+} from '@chakra-ui/react';
+import { Container } from '../components/Container';
+import { CTA } from '../components/CTA';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { Footer } from '../components/Footer';
+import { Hero } from '../components/Hero';
+import { Main } from '../components/Main';
+import { withApollo } from '../utils/withApollo';
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+const Index = () => {
+	return (
+		<Container height='100vh'>
+			<Hero />
+			<Main>
+				<Text>
+					Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
+					<Code>typescript</Code>.
+				</Text>
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
-      </Text>
+				<List spacing={3} my={0}>
+					<ListItem>
+						<ListIcon as={CheckCircleIcon} color='green.500' />
+						<ChakraLink
+							isExternal
+							href='https://chakra-ui.com'
+							flexGrow={1}
+							mr={2}
+						>
+							Chakra UI <LinkIcon />
+						</ChakraLink>
+					</ListItem>
+					<ListItem>
+						<ListIcon as={CheckCircleIcon} color='green.500' />
+						<ChakraLink
+							isExternal
+							href='https://nextjs.org'
+							flexGrow={1}
+							mr={2}
+						>
+							Next.js <LinkIcon />
+						</ChakraLink>
+					</ListItem>
+				</List>
+			</Main>
+			<DarkModeSwitch />
+			<Footer>
+				<Text>Next ❤️ Chakra</Text>
+			</Footer>
+			<CTA />
+		</Container>
+	);
+};
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
-
-export default Index
+export default withApollo({ ssr: true })(Index);
