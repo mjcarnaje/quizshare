6e05e2073c0ea0b57b3a4eb2eaa4fc9636280613
Michael James Carnaje <michaeljamescaranje1@gmail.com>
2021-02-23 17:28:35 +0800

    search bar ui

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,13 +1,19 @@
-import { SearchIcon } from '@chakra-ui/icons';
+import { CloseIcon, SearchIcon } from '@chakra-ui/icons';
 import {
-	Box,
 	Flex,
 	Input,
 	InputGroup,
-	InputLeftElement,
+	InputRightElement,
+	IconButton,
+	MenuButton,
+	Menu,
+	MenuItem,
+	MenuList,
+	useColorModeValue,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
+import { HiOutlineFilter } from 'react-icons/hi';
 
 export const SearchBar: React.FC = () => {
 	const router = useRouter();
@@ -15,9 +21,17 @@ export const SearchBar: React.FC = () => {
 	const [text, setText] = useState('');
 
 	return (
-		<Flex justify='center' align='center' w='full' h='30vh'>
-			<Box w='50%'>
+		<Flex
+			justify='center'
+			align='center'
+			w='full'
+			h='25vh'
+			bg={useColorModeValue('', '')}
+			mb='10vh'
+		>
+			<Flex align='center' w='50%'>
 				<form
+					style={{ flex: 1 }}
 					onSubmit={(e) => {
 						e.preventDefault();
 						router.push({
@@ -26,21 +40,73 @@ export const SearchBar: React.FC = () => {
 						});
 					}}
 				>
-					<InputGroup size='lg' variant='filled' borderRadius='md'>
-						<InputLeftElement
-							pointerEvents='none'
-							children={<SearchIcon color='gray.300' />}
-						/>
+					<InputGroup size='lg' variant='outline'>
 						<Input
 							onChange={(e) => setText(e.target.value)}
 							value={text}
 							type='text'
-							placeholder='Search'
-							_focus={{ shadow: 'outline' }}
+							placeholder='Search...'
+							borderRadius='lg'
+							boxShadow='sm'
+							fontFamily='poppins'
+							background={useColorModeValue('#fff', 'rgb(38, 38, 38)')}
+							_focus={{
+								outline: 'none',
+								boxShadow: 'md',
+								background: useColorModeValue('#fff', 'rgb(38, 38, 38)'),
+							}}
+							_hover={{
+								background: useColorModeValue('#fff', 'rgb(38, 38, 38)'),
+							}}
+							flex='1'
+						/>
+						<InputRightElement
+							cursor={text.length === 0 ? '' : 'pointer'}
+							onClick={() => {
+								if (text.length === 0) return;
+								setText('');
+							}}
+							children={
+								text.length === 0 ? (
+									<SearchIcon color='gray.300' />
+								) : (
+									<CloseIcon fontSize='14px' color='gray.300' />
+								)
+							}
 						/>
 					</InputGroup>
 				</form>
-			</Box>
+
+				<Menu>
+					<MenuButton
+						as={IconButton}
+						background={useColorModeValue('#fff', 'rgb(38, 38, 38)')}
+						boxShadow='sm'
+						color='gray.300'
+						aria-label='Filter'
+						borderRadius='lg'
+						borderWidth='1px'
+						icon={<HiOutlineFilter />}
+						_focus={{
+							outline: 'none',
+							boxShadow: 'md',
+							background: useColorModeValue('#fff', 'rgb(38, 38, 38)'),
+						}}
+						_hover={{
+							background: useColorModeValue('#fff', 'rgb(38, 38, 38)'),
+						}}
+						_active={{
+							background: useColorModeValue('#fff', 'rgb(38, 38, 38)'),
+						}}
+						size='lg'
+						ml='15px'
+					/>
+					<MenuList w='232px'>
+						<MenuItem>Users</MenuItem>
+						<MenuItem>Quizzes</MenuItem>
+					</MenuList>
+				</Menu>
+			</Flex>
 		</Flex>
 	);
 };
