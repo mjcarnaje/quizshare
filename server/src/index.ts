4f4ee8421cb703cdc053955fbd5810cea4ccf796
unknown <michaeljamescarnaje1@gmail.com>
2021-09-26 11:07:43 +0800

    Revert ":bug: added secure in cookie"
    This reverts commit 169aa1c24350f80b55f0fcbb8f5db6e5341fd6c6.

--- server/src/index.ts
+++ server/src/index.ts
@@ -116,7 +116,7 @@ const main = async () => {
         saveUninitialized: false,
         cookie: {
           httpOnly: true,
-          secure: __PROD__,
+          // secure: __PROD__,
           maxAge: oneWeekInMs,
           sameSite: "none",
           domain: ".api-quizshare.herokuapp.com",
